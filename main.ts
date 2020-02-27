cb96bd6e5cfbae05780ffd51434241e1c09f9586
javdel000 <58610616+javdel000@users.noreply.github.com>
2020-02-27 13:27:03 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+/**
+ * Beef loaf
+ */
+/**
+ * Corona
+ */
+/**
+ * Pork
+ */
+/**
+ * chicken
+ */
+// steak
+let food0 = "beefloaf"
+let food1 = "Corona"
+let food2 = "Steak"
+let food3 = "Chicken"
+let food4 = "Pork"
+let listFoods = ["beefloaf", "Corona", "Steak", "Chicken", "Pork"]
+game.splash(listFoods[0])
+game.splash(listFoods[3])
+game.splash(listFoods[4])
